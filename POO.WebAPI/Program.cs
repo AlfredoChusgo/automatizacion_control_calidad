ce2a60e9439bb9813ccc0da90cb86b384038f62f
AlfredoCh <alfredochusgodev@gmail.com>
2024-01-23 09:30:17 -0400

    Clean up

--- POO.WebAPI/Program.cs
+++ POO.WebAPI/Program.cs
@@ -1,28 +1,21 @@
-using Microsoft.EntityFrameworkCore;
 using POO.Application;
 using POO.Infrastructure;
-using POO.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
-
 {
 
     builder.Services        
         .AddApplication()
         .AddInfrastructure();
 }
-//builder.Services.AddDbContext<ApplicationDbContext>(
-//                options => options.UseSqlite("Data Source=app.db", x => x.MigrationsAssembly("POO.WebAPI")));
-
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -32,9 +25,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-//app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
